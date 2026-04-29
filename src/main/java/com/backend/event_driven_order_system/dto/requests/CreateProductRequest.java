b81a865a01fc59f21442871cc9dc9f33ac210464
Bernardo Giudicelli <giudicelli.be@gmail.com>
2026-04-29 08:15:12 -0300

    refactor(dto): improve product request validation constraints

--- src/main/java/com/backend/event_driven_order_system/dto/requests/CreateProductRequest.java
+++ src/main/java/com/backend/event_driven_order_system/dto/requests/CreateProductRequest.java
@@ -1,4 +1,24 @@
 package com.backend.event_driven_order_system.dto.requests;
 
-public record CreateProductRequest() {
+import jakarta.validation.constraints.*;
+import java.math.BigDecimal;
+
+public record CreateProductRequest(
+
+        @NotBlank(message = "Name must not be blank")
+        @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+        String name,
+
+        @NotBlank(message = "Description must not be blank")
+        @Size(max = 500, message = "Description must have max 500 characters")
+        String description,
+
+        @NotNull(message = "Price is required")
+        @Positive(message = "Price must be greater than 0")
+        BigDecimal price,
+
+        @NotNull(message = "Stock is required")
+        @Min(value = 0, message = "Stock cannot be negative")
+        Integer stock
+) {
 }
