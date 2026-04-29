419a51e43afa2262b1c0c29bcb46dedbc21a8caa
Bernardo Giudicelli <giudicelli.be@gmail.com>
2026-04-29 04:42:55 -0300

    feat(auth): implement authentication controller endpoints

--- src/main/java/com/backend/event_driven_order_system/controller/AuthController.java
+++ src/main/java/com/backend/event_driven_order_system/controller/AuthController.java
@@ -1,4 +1,32 @@
 package com.backend.event_driven_order_system.controller;
 
+import com.backend.event_driven_order_system.dto.requests.LoginRequest;
+import com.backend.event_driven_order_system.dto.requests.RegisterRequest;
+import com.backend.event_driven_order_system.dto.responses.LoginResponse;
+import com.backend.event_driven_order_system.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        authService.register(request);
+        return ResponseEntity.ok("Usuário criado com sucesso");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
+        String token = authService.login(request.email(), request.password());
+        return ResponseEntity.ok(new LoginResponse(token));
+    }
 }
